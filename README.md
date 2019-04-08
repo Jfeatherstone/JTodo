3d4a49a0e0a700bf5abfad796a389148af6fc946
Jack <jfeatherstone123@gmail.com>
2019-04-08 15:45:13 -0400

    Updated readme with some installation info

--- README.md
+++ README.md
@@ -26,3 +26,14 @@ $ todo --toggle-color					# Enable or disable color mode
 $ todo --toggle-groups						# Enable or disable group display mode
 $ todo --completed							# Print the ten most recently completed tasks
 ```
+
+## [Installation]
+Download the [latest release](https://github.com/Jfeatherstone/jTodo/releases/tag/2.0) and unzip it to the directory of your choice. Run the install script (make sure you don't move the jar file into a different directory from the script) which should delete itself afterwards.
+```
+unzip Todo.jar && ./install.sh
+```
+This will generate the folder ~/.config/jTodo/ with the jar file and uninstall script inside, along with the todo file in /usr/bin/ to call the jar file. On the first time running the program, the config creation wizard will run to setup the list file in the same directory.
+
+## [Configuration]
+
+
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/zsh
 # This is an install script for jTodo
 # If you have any questions/concerns, you can contact me at:
 # jfeatherstone123@gmail.com
@@ -7,20 +7,45 @@
 # If you don't you should change this variable to wherever you want to
 # store your todolist and the jar file for jTodo
 
-dir = "/home/$USER/.config/jTodo"
-if [! -d "$dir" ]; then
-		mkdir -p "$dir"
+
+configDir="/home/$USER/.config/jTodo/"
+if [ ! -d "$configDir" ]; then
+		mkdir -p "$configDir"
 fi
 
-# Compiling
-cd "$dir"
-if [ -d "bin/" ]; then
-		# Clean the folder
-		rm -r bin/
-fi
+# Move the already compiled jar to the proper location
+# When downloaded, the jar should be in the same directory as this file
+mv jTodo.jar "$configDir"
 
-# Now make the folder
-mkdir bin/
+cd "$configDir"
 
-javac src/com/jfeather/App/*.java -d bin/
+# Now create the run script
+touch todo
+echo "
+cd /home/$USER/.config/jTodo/
+java -jar jTodo.jar \$@
+cd \$OLDPWD
+" >> todo
 
+# Now move the file to /usr/bin so it can be run
+sudo mv todo /usr/bin/todo
+
+# Now allow everyone to execute the file
+chmod u+x,g+x,o+x /usr/bin/todo
+
+# Now create the uninstall script
+touch uninstall.sh
+echo "
+sudo rm /usr/bin/todo
+rm -r /home/\$USER/.config/jTodo/
+echo \"Config folder and command removed! If you added todo to your bashrc or zshrc file, be sure to remove it now!\"
+" >> uninstall.sh
+
+# Now allow it to be executed by anyone
+chmod u+x,g+x,o+x uninstall.sh
+
+# Now tell the user the process is done
+echo "Installation completed successfully, deleteing install file now..."
+
+# Now remove this file
+#rm install.sh
